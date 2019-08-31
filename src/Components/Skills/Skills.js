e613765f6a9795731218c51dbfd33ed9d52eca1e
Jacob Haas <jacob_haas_7@hotmail.com>
2019-08-31 11:41:50 -0700

    skills component positioning, start of animations

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -24,85 +24,85 @@ import mysql from "../../Assests/Images/Skills/mysql.png";
 import mongodb from "../../Assests/Images/Skills/mongodb.png";
 import firebase from "../../Assests/Images/Skills/firebase.png";
 
+class Skills extends React.Component {
+  render() {
+    return (
+      <div className="skillsWrapper">
 
-class Skills extends React.Component{
-    render(){
-        return(
-            <div>
+          
+        <div className="databaseWrapper">
+          <h1>Database</h1>
+          <div class="database">
+            <img src={mysql} className="skillsImg imgResize" alt="mysql"></img>
+            <div></div>
+            <img
+              src={mongodb}
+              className="skillsImg mongoImg"
+              alt="mongodb"
+            ></img>
 
+            <div></div>
+            <img
+              src={firebase}
+              className="skillsImg firebaseImg imgResize"
+              alt="firebase"
+            ></img>
+            <div></div>
+          </div>
+        </div>
 
-                <div className="frontEndWrapper">
+        <div className="frontEndWrapper">
+          <h1>Front-End</h1>
+          <div className="frontEnd">
+            <div></div>
+            <div></div>
+            <img src={bootstrap} className="skillsImg" alt="bootstrap"></img>
+            <div></div>
+            <div></div>
 
-                <h1>Front-End</h1>
-                <div className="frontEnd">
-                    
-                    <div></div><div></div>
-                    <img src={bootstrap} className="skillsImg" alt="bootstrap"></img>
-                    <div></div><div></div>
-                    
-                    <div></div>
-                    <img src={css} className="skillsImg" alt="css"></img>
-                    <div></div>
-                    <img src={cssgrid} className="skillsImg" alt="cssgrid"></img>
-                    <div></div>
+            <div></div>
+            <img src={css} className="skillsImg" alt="css"></img>
+            <div></div>
+            <img src={cssgrid} className="skillsImg" alt="cssgrid"></img>
+            <div></div>
 
-                    <img src={html} className="skillsImg" alt="html"></img>
-                    <div></div>
-                    <img src={javascript} className="skillsImg" alt="javascript"></img>
-                    <div></div>
-                    <img src={jquery} className="skillsImg" alt="jquery"></img>
+            <img src={html} className="skillsImg" alt="html"></img>
+            <div></div>
+            <img src={javascript} className="skillsImg" alt="javascript"></img>
+            <div></div>
+            <img src={jquery} className="skillsImg" alt="jquery"></img>
 
-                    <div></div>
-                    <img src={materialui} className="skillsImg" alt="materialui"></img>
-                    <div></div>
-                    <img src={sass} className="skillsImg" alt="sass"></img>
-                    <div></div>
+            <div></div>
+            <img src={materialui} className="skillsImg" alt="materialui"></img>
+            <div></div>
+            <img src={sass} className="skillsImg" alt="sass"></img>
+            <div></div>
 
-                    <div></div><div></div>
-                    <img src={react} className="skillsImg" alt="react"></img>
-                </div>
-                </div>
+            <div></div>
+            <div></div>
+            <img src={react} className="skillsImg" alt="react"></img>
+          </div>
+        </div>
 
+        <div className="backEndWrapper">
+          <h1>Back-End</h1>
+          <div className="backEnd">
+            <div></div>
+            <img src={ajax} className="skillsImg imgResize" alt="ajax"></img>
+            <div></div>
 
+            <img src={api} className="skillsImg imgResize" alt="api"></img>
+            <div></div>
+            <img src={json} className="skillsImg imgResize" alt="json"></img>
 
-                <div className="backEndWrapper">
-                <h1>Back-End</h1>
-                <div className="backEnd">
-                    <div></div>
-                    <img src={ajax} className="skillsImg" alt="ajax"></img>
-                    <div></div>
-
-                    <img src={api} className="skillsImg" alt="api"></img>
-                    <div></div>
-                    <img src={json} className="skillsImg" alt="json"></img>
-
-                    <div></div>
-                    <img src={node} className="skillsImg" alt="node"></img>
-                    <div></div>
-                </div>
-                </div>
-
-
-                <div className="databaseWrapper">
-                    <h1>Database</h1>
-                    <div class="database">
-
-                    <img src={mysql} className="skillsImg" alt="mysql"></img>
-                    <div></div>
-                    <img src={mongodb} className="skillsImg" alt="mongodb"></img>
-                    
-                    <div></div>
-                    <img src={firebase} className="skillsImg" alt="firebase"></img>
-                    <div></div>
-                    </div>
-                </div>
-
-
-
-
-            </div>
-        )
-    }
+            <div></div>
+            <img src={node} className="skillsImg imgResize" alt="node"></img>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Skills;
